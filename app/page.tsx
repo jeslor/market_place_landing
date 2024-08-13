b40ecf37d6d5a066112ae9e8e8001a187da70378
twani <jbestius@gmail.com>
2024-08-13 16:49:24 +0300

    complete the design

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import CategoryIcon from '@/components/categoryIcon';
 
 const Landing = () => {
   return (
-    <div className="min-h-screen h-full bg-primary2 grid items-center fixed w-full">
+    <div className="min-h-screen h-full bg-primary2 flex flex-col w-full items-center fixed w-full">
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="bg-clip-text text-transparent bg-gradient-radial from-primary9 to-primary1  text-[3rem] uppercase font-bold">nusoura fashions</h1>
-        <div style={{gridTemplateColumns:`repeat(${products.length}, 1fr)`}} className={`grid grid-cols-${products.length}  pt-[10rem]`}>
+        <div style={{gridTemplateColumns:`repeat(${products.length}, 1fr)`}} className={`grid grid-cols-${products.length}  pt-[14rem] gap-[4rem]`}>
             {
                 products.map((product, index) => (
                   <CategoryIcon product={product} position={index+1} totalProducts={products.length}/>
@@ -17,6 +17,10 @@ const Landing = () => {
             }
         </div>
       </div>
+      <p className="text-primary7 text-[12px] flex justify-between px-8 w-full py-4 ">
+        <span>© 2024 Jeslor.com, All rights reserved</span>
+        <span>privacy | Security</span>
+      </p>
     </div>
   )
 }
--- components/categoryIcon.tsx
+++ components/categoryIcon.tsx
@@ -14,12 +14,12 @@ const CategoryIcon = ({product, position, totalProducts}:{
     if (position > middlePosition) {
         position = totalProducts - position + 1;
     }
-    displacementPosition = -(middlePosition - position) * 4;
+    displacementPosition = -(middlePosition - position) * 5;
     
   return (
-    <div style={{transform:`translateY(${displacementPosition}rem)`}} className={`text-primary10 mx-10  flex flex-col items-center`}>
-        <Icon className="text-[3rem]" icon={product.icon} />
-        <p className="text-[14px] opacity-65 pt-2 capitalize">{product.name}</p>
+    <div style={{transform:`translateY(${displacementPosition}rem)`}} className={`text-primary10 flex flex-col items-center justify-center h-[100px] w-[100px] hover:bg-gradient-to-t from-primary1 to-primary2 rounded-[50%] shadow-2xl cursor-pointer`}>
+        <Icon className="text-[2.5rem] h-[30%]" icon={product.icon} />
+        <p className="text-[12px] opacity-65 pt-2 capitalize">{product.name}</p>
     </div>
   )
 }
