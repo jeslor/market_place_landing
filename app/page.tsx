1a9ff3a64fb8dcfffc9a6a36fe5bca370f2aadda
twani <jbestius@gmail.com>
2024-08-14 11:02:43 +0300

    add animation to the individual icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { useEffect, useState } from "react";
+"use server";
 import {products} from "@/constants/icons";
 import CategoryIcon from '@/components/categoryIcon';
 
@@ -9,39 +8,23 @@ import CategoryIcon from '@/components/categoryIcon';
 
 
 const Landing = () => {
-  const [positionState, setPositionState] = useState({
-    x: 0,
-    y: 0
-  });
-  const [newStyles, setNewStyles] = useState({
-  });
+ 
 
-  useEffect(() => {
-    const  fashionHeader = document.getElementById('fashionHeader');
-    if(!fashionHeader) return;
-    const fashionHeaderRect = fashionHeader.getBoundingClientRect();
-    console.log(fashionHeaderRect);
+  // if (typeof window === 'undefined') {
+  //   return null;
+  //   const InitalPosition = document.getElementById('fashionHeader')?.getBoundingClientRect();
+  //   const positionY = InitalPosition?.y;
+  //   const positionX = InitalPosition?.x;
+  //   const newStyles  = {
     
-    const fashionHeaderX = fashionHeaderRect.x;
-    const fashionHeaderY = fashionHeaderRect.y;
-    setPositionState({
-      x: fashionHeaderX,
-      y: fashionHeaderY
-    });
-  },[positionState.x, positionState.y]);
-
-  useEffect(() => {
-    setNewStyles({
-      background: `radial-gradient(circle at ${positionState.x}px ${positionState.y}px, #726240, #2a2416)`
-    });
-  },[positionState]);
+  // }
   
 
 
   return (
-    <div style={newStyles} className="min-h-screen h-full  flex flex-col w-full items-center fixed ">
+    <div style={{background:"radial-gradient(circle at center, #726240, transparent"}}  className="min-h-screen h-full  flex flex-col w-full items-center fixed">
       <div className="flex flex-col items-center justify-center h-full">
-        <h1 id="fashionHeader" className="bg-clip-text text-transparent bg-gradient-radial from-primary9 to-primary1  text-[3rem] uppercase font-bold">nusoura fashions</h1>
+        <h1 id="fashionHeader" className="bg-clip-text text-transparent bg-gradient-radial from-primary9 to-primary3  text-[3rem] uppercase font-bold">nusoura fashions</h1>
         <div style={{gridTemplateColumns:`repeat(${products.length}, 1fr)`}} className={`flex flex-wrap tablet:grid grid-cols-${products.length}  pt-[14rem] gap-4 laptop:gap-[2.8rem] desktop:gap-[4rem]`}>
             {
                 products.map((product, index) => (
--- components/categoryIcon.tsx
+++ components/categoryIcon.tsx
@@ -30,7 +30,7 @@ const [displacementPosition, setDisplacementPosition] = useState(0);
     })
     
   return (
-    <div style={{transform:`translateY(${displacementPosition}rem)`}} className={`text-primary10 flex flex-col font-semibold items-center justify-center h-[80px] laptop:h-[90px] desktop:h-[100px] w-[80px] laptop:w-[90px] desktop:w-[100px] hover:bg-gradient-to-t from-primary1 to-primary2 rounded-[50%] shadow-2xl cursor-pointer t`}>
+    <div style={{transform:`translateY(${displacementPosition}rem)`, transitionDelay:`${position/3}s`, transitionProperty:"transform", transitionTimingFunction:"ease-out", transitionDuration:"200ms"}} className={`text-primary10 flex flex-col font-semibold items-center justify-center h-[80px] laptop:h-[90px] desktop:h-[100px] w-[80px] laptop:w-[90px] desktop:w-[100px] hover:bg-gradient-to-t from-primary1 to-primary2 rounded-[50%] shadow-2xl cursor-pointer border-[1px] border-primary4 transition-all`}>
         <Icon className=" text-[1.5rem] laptop:text-[2.5rem] h-[30%]" icon={product.icon} />
         <p className="text-[12px] opacity-65 pt-2 capitalize">{product.name}</p>
     </div>
