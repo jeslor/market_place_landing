3948c47ca16862893ef5878d3ee9657e25f41052
twani <jbestius@gmail.com>
2024-08-16 23:59:18 +0300

    feat: add the functionality to reduce the radial gradient size on individual icon hover

--- components/categoryIcon.tsx
+++ components/categoryIcon.tsx
@@ -5,7 +5,8 @@ import { Icon } from '@iconify/react';
 const CategoryIcon = ({product, position, totalProducts, setGradientRadius, setCurrGradientPosition, currGradientPosition}:{
     product:{
         name:string,
-        icon:string
+        icon:string,
+        id:string
     },
     position:number
     totalProducts:number,
@@ -33,7 +34,25 @@ const [displacementPosition, setDisplacementPosition] = useState(0);
 
     useEffect(() => {
         const handleMouseMove = (e:any) => {
-         constIconPosition = document.getElementById(`categoryIcon${position}`)?.getBoundingClientRect();
+            let currElement = document.getElementById(`categoryIcon${product.id}`);
+            // if (currElement) {
+            //     let rect = currElement.getBoundingClientRect();
+            //    const ClientY = e.clientY;
+            //     const ClientX = e.clientX;
+            //     if(ClientY < rect.top || ClientY > rect.bottom || ClientX < rect.left || ClientX > rect.right){
+            //        setGradientRadius(400);
+            //     }else{
+            //         setGradientRadius(150)
+                    
+            //     }
+                
+            // }
+            currElement?.addEventListener('mouseover', () => {
+                setGradientRadius(120);
+            })
+            currElement?.addEventListener('mouseleave', () => {
+                setGradientRadius(400);
+            })
           setCurrGradientPosition({
             x: `${e.clientX}px`,
             y: `${e.clientY}px`
@@ -41,13 +60,14 @@ const [displacementPosition, setDisplacementPosition] = useState(0);
         }
       
         window.addEventListener('mousemove', handleMouseMove);
-
-
+        return () => {
+          window.removeEventListener('mousemove', handleMouseMove);
+        }
         
        }, [currGradientPosition.x, currGradientPosition.y])
     
   return (
-    <div id={`categoryIcon${position}`} style={{transform:`translateY(${displacementPosition}rem)`, transitionDelay:`${position/3}s`, transitionProperty:"transform", transitionTimingFunction:"ease-out", transitionDuration:"200ms"}} className={`text-primary10 flex flex-col font-semibold items-center justify-center h-[80px] laptop:h-[90px] desktop:h-[100px] w-[80px] laptop:w-[90px] desktop:w-[100px] hover:bg-gradient-to-t from-primary1 to-primary2 rounded-[50%] shadow-2xl cursor-pointer border-[1px] border-primary4 transition-all`}>
+    <div id={`categoryIcon${product.id}`} style={{transform:`translateY(${displacementPosition}rem)`, transitionDelay:`${position/3}s`, transitionProperty:"transform", transitionTimingFunction:"ease-out", transitionDuration:"200ms"}} className={`text-primary10 flex flex-col font-semibold items-center justify-center h-[80px] laptop:h-[90px] desktop:h-[100px] w-[80px] laptop:w-[90px] desktop:w-[100px] hover:bg-gradient-to-t from-primary1 to-primary2 rounded-[50%] shadow-2xl cursor-pointer border-[1px] border-primary4 transition-all hover:scale-110`}>
         <Icon className=" text-[1.5rem] laptop:text-[2.5rem] h-[30%]" icon={product.icon} />
         <p className="text-[12px] opacity-65 pt-2 capitalize">{product.name}</p>
     </div>
