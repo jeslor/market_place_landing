77ebab2e4c96a8e55c2471784fac3e64836d9e79
twani <jbestius@gmail.com>
2024-08-20 12:43:49 +0200

    fix: resiponsiveness final commit

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ const Landing = () => {
   return (
     <div id="mainEntry" style={newStyle}  className="min-h-screen h-full  flex flex-col w-full items-center fixed">
       <div className="flex flex-col items-center justify-center h-full">
-        <h1 id="fashionHeader" className="bg-clip-text text-transparent bg-gradient-radial from-primary9 to-primary2  text-[3rem] uppercase font-bold">nusoura fashions</h1>
-        <div style={{gridTemplateColumns:`repeat(${products.length}, 1fr)`}} className={`flex flex-wrap tablet:grid grid-cols-${products.length} pt-12 px-4  tablet:pt-[14rem] gap-4 laptop:gap-[2.8rem] desktop:gap-[4rem]`}>
+        <h1 id="fashionHeader" className="bg-clip-text text-transparent bg-gradient-radial from-primary9 to-primary2  text-[2rem] tablet:text-[3rem] uppercase font-bold">nusoura fashions</h1>
+        <div style={{gridTemplateColumns:`repeat(${products.length}, 1fr)`}} className={`flex flex-wrap justify-center tablet:grid grid-cols-${products.length} pt-12 px-4  tablet:pt-[14rem] gap-4 laptop:gap-[2.8rem] desktop:gap-[4rem]`}>
             {
                 products.map((product:any, index) => (
                   <CategoryIcon key={index} product={product} position={index+1} totalProducts={products.length} currGradientPosition={currGradientPosition} gradientRadius={gradientRadius} setGradientRadius={setGradientRadius} setCurrGradientPosition={setCurrGradientPosition}/>
@@ -39,7 +39,7 @@ const Landing = () => {
             }
         </div>
       </div>
-      <p className="text-primary7 text-[12px] flex justify-between px-8 w-full py-4 ">
+      <p className="text-primary7 text-[12px] flex justify-between flex-col tablet:flex-row px-4 tablet:px-8 w-full py-4 ">
         <span>© 2024 Jeslor.com, All rights reserved</span>
         <span>privacy | Security</span>
       </p>
